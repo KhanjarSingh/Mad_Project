70773874a2a27df9cffd87ce1106401c1b57941b
Parth <partht0347@gmail.com>
2025-09-29 22:18:54 +0530

    Add user registration endpoint to server
    Introduced a POST /users route for user registration using Prisma ORM. The endpoint validates required fields, checks for existing email, hashes the password, and creates a new user in the database.

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,58 @@
 const express = require('express')
 const process = require('process')
 const dotenv = require('dotenv')
-
+const {PrismaClient} = require('./src/generated/prisma')
+const { error } = require('console')
 dotenv.config()
 
 
 const app = express()
-
+const prisma = new PrismaClient()
 app.get('/',(req,res)=>{
     res.status(200).send("Server is Running Perfectly")
+})
+
+// User API Routes
+
+app.post('/users', async(req,res)=>{
+    const {name,email,password,phone} = req.body
+
+    if (!name || !email || !password){
+        return res.status(401).json({ error: "All Fields are required" })
+    }
+
+    const checkEmail = await prisma.users.findFirst({
+        where:{
+            email:email
+        }
+    })
+    if (checkEmail){
+        return res.status(401).json({error:"The Email Already Exists"})
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+
+    const result = await prisma.users.create({
+        data:{
+            name:name,
+            email:email,
+            password:hashedPassword,
+            phone:phone,
+        }
+    })
+    
+    res.status(201).json({
+        id:result.id,
+        name:result.name,
+        email:result.email,
+        password:result.password,
+        phone:result.phone
+    })
 })
 
 
 app.listen(process.env.PORT,()=>{
     console.log(` Server is running at http://localhost:${process.env.PORT}`)
 })
+
